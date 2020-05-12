4664ced4cda963f6652a932c1e3586656d32743c
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-12 12:04:47 +0200

    Fix: shorten long names

--- plugins/modules/axios.js
+++ plugins/modules/axios.js
@@ -2,14 +2,14 @@ export default ({ store, $axios, redirect }) => {
   /**
    * set global accessToken on refresh
    */
-  $axios.setToken(store.state.accessToken, 'Bearer')
+  $axios.setToken(store?.state?.accessToken, 'Bearer')
 
   /**
    * Logout if user has no right credentials
    * TODO: Temporary Workaround
    */
   $axios.onError((error) => {
-    if (error.response.status === 401) {
+    if (error?.response?.status === 401) {
       // store.dispatch('resetUser')
       // redirect('/login')
     }
