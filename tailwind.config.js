728a280c940dad002ae28940f39e16aaa8a2248e
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-13 19:24:18 +0200

    Fix: Spinner coloer

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,7 +68,7 @@ module.exports = {
     },
     spinner: (theme) => ({
       dark: {
-        color: theme('colors.primary', 'currentColor'), // color you want to make the spinner
+        color: theme('colors.light', 'currentColor'), // color you want to make the spinner
         size: '1em', // size of the spinner (used for both width and height)
         border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
         speed: '500ms', // the speed at which the spinner should rotate
