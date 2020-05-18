06c3da6f45fc0ed9a99e06deef0055a3b86adf25
Lukas Mauser <l.mauser.dev@gmail.com>
2020-05-18 22:20:05 +0200

    Fix: disable bugsnag logs in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -210,6 +210,7 @@ export default {
         config: {
           apiKey: process.env.BUGSNAG_API_KEY,
           releaseStage: 'development',
+          logger: process.env.NODE_ENV === 'production' ? null : () => {},
           notifyReleaseStages: ['production', 'staging'],
           enabledReleaseStages: ['production', 'staging'],
         },
