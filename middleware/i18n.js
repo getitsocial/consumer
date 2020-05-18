4c91f60264ebeadc098d1bc7aa7cbccb42b0bcf0
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-18 16:43:36 +0200

    Fix: Test issue on meta tags

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -21,8 +21,17 @@ export default function ({
         req.headers['accept-language']
           .split(',')[0]
           .toLocaleLowerCase()
-          .substring(0, 2) ||
-        'en'
+          .substring(0, 2)
+
+      // Test Issue on meta tags
+      // https://github.com/nuxt/nuxt.js/issues/2034
+      if (req.headers['accept-language'] !== undefined) {
+        const locale =
+          req.headers['accept-language']
+            .split(',')[0]
+            .split('-')[0]
+            .toLocaleLowerCase() || 'en'
+      }
 
       store.commit('setLang', locale)
       i18n.locale = store.state.locale
