38c4cfe4ca8a399773f5a6c1849a1975d02fa46a
Lukas Mauser <l.mauser.dev@gmail.com>
2020-05-29 15:25:51 +0200

    Fix: i18 middleware

--- middleware/i18n.js
+++ middleware/i18n.js
@@ -15,12 +15,14 @@ export default function ({
   if (req) {
     if (route.name) {
       // Get locale from cookie or from useragend
-      const locale =
-        $cookies.get('locale') ||
+
+      const headersLocale =
         req.headers['accept-language']
-          .split(',')[0]
-          .toLocaleLowerCase()
-          .substring(0, 2)
+          ?.split(',')[0]
+          ?.toLocaleLowerCase()
+          ?.substring(0, 2) || 'en'
+
+      const locale = $cookies.get('locale') || headersLocale
 
       store.commit('setLang', locale)
       i18n.locale = store.state.locale
