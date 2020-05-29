10a853efeb120f47e7f0459056800cbc81bb45b2
Lukas Mauser <l.mauser.dev@gmail.com>
2020-05-29 15:07:00 +0200

    Fix: testpage for og

---
+++ pages/testpage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="padding-top: 5em;">
+    <h1>Testpage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ seo }) {
+    seo({
+      name: 'Testpage',
+      title: 'lokal auf get it!',
+      templateTitle: '%name% - %title%',
+      description: 'Lokal einkaufen!',
+      og: {
+        title: 'Testpage',
+        image: 'shop.picture.url',
+        locale: 'de',
+      },
+      fb: {
+        appId: '574950416384081',
+      },
+    })
+  },
+}
+</script>
+
+<style lang="css" scoped></style>
